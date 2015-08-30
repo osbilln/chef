910583169be227db6e35d1823019583f14d18e4a
Mark Mastoras <mmastoras@mac.com>
2015-08-30 12:52:23 -0700

    use internal dns for db hosts

--- nh-nagios-0.2.0/files/default/drdb1_check_max_ids.sh
+++ nh-nagios-0.2.0/files/default/drdb1_check_max_ids.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$#" -ne 6 ]; then
-    echo "drdb1_check_max_ids.sh usage: <repl_db_host> <repl_db_user> <repl_db_pw> <master_db_host> <master_db_user> <master_db_pw>"
-    exit
+    echo "drdb1_check_max_ids.sh invalid args $# usage: <repl_db_host> <repl_db_user> <repl_db_pw> <master_db_host> <master_db_user> <master_db_pw>"
+    exit 1
 fi
 
 REPL_DB_HOST=$1 
@@ -12,7 +12,7 @@ MASTER_DB_HOST=$4
 MASTER_DB_USER=$5
 MASTER_DB_PW=$6
 
-dbs=("salesdemo" "dmiplatform" "dmiplatform_bonita_core" "dmiplatform_history" "dmiplatform_staging")
+dbs=("salesdemo" "dmiplatform" "dmiplatform_staging")
 
 ## now loop through the above array
 for i in "${dbs[@]}"
@@ -20,8 +20,9 @@ do
     echo "checking ids on $i"
     status=`/usr/lib/nagios/plugins/check_max_id.rb $i $REPL_DB_HOST $REPL_DB_USER $REPL_DB_PW $MASTER_DB_HOST $MASTER_DB_USER $MASTER_DB_PW`
     if [ status -ne 0]; then
-        exit(1)
+        exit 1
     fi
 done
 
-exit(0)
+echo "max ids match between drdb1 and db5"
+exit 0
--- nh-nagios-0.2.0/files/default/drdb2_check_max_ids.sh
+++ nh-nagios-0.2.0/files/default/drdb2_check_max_ids.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 6 ]; then
     echo "drdb2_check_max_ids.sh usage: <repl_db_host> <repl_db_user> <repl_db_pw> <master_db_host> <master_db_user> <master_db_pw>"
-    exit
+    exit 1
 fi
 
 REPL_DB_HOST=$1 
@@ -12,8 +12,7 @@ MASTER_DB_HOST=$4
 MASTER_DB_USER=$5
 MASTER_DB_PW=$6
 
-#dbs=("citinew_staging" "comcastprod" "comcastprod_bonita_core" "comcastprod_bonita_history" "comcastprod_staging" "coxprod" "coxprod_bonita_core" "coxprod_bonita_history" "coxprod_staging" "usbankprod" "usbankprod_bonita_core" "usbankprod_bonita_history" "usbankprod_staging")
-dbs=("usbankprod" "usbankprod_bonita_core" "usbankprod_bonita_history" "usbankprod_staging")
+dbs=("citinew_staging" "comcastprod" "comcastprod_staging" "coxprod" "coxprod_staging" "usbankprod" "usbankprod_staging")
 
 ## now loop through the above array
 for i in "${dbs[@]}"
@@ -21,8 +20,9 @@ do
     echo "checking ids on $i"
     status=`/usr/lib/nagios/plugins/check_max_id.rb $i $REPL_DB_HOST $REPL_DB_USER $REPL_DB_PW $MASTER_DB_HOST $MASTER_DB_USER $MASTER_DB_PW`
     if [ status -ne 0]; then
-        exit(1)
+        exit 1
     fi
 done
 
-exit(0)
+echo "max ids match between drdb2 and db7"
+exit 0
