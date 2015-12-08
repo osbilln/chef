8cbf35ee502b731eee8aa1ee894c9f4bd1850aac
Mark Mastoras <mmastoras@mac.com>
2015-12-08 15:42:54 -0800

    set number of tables to dump at a time

--- replication_scripts/db_tables_from_file_dump.rb
+++ replication_scripts/db_tables_from_file_dump.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 if ARGV.count != 7
-  puts "db_tables_from_file_dump.rb usage: <db_name> <db_host> <db_user> <db_pw> <dump dir> <bucket location> <tables to dump>"
+  puts "db_tables_from_file_dump.rb usage: <db_name> <db_host> <db_user> <db_pw> <dump dir> <tables to dump> <max tables per dump>"
   exit
 end
 
@@ -10,10 +10,10 @@ DB_HOST = ARGV[1]
 DB_USER = ARGV[2]
 DB_PW = ARGV[3]
 DUMP_DIR = ARGV[4]
-S3_BUCKET = ARGV[5]
-TABLES_TO_DUMP_FILE = ARGV[6]
+TABLES_TO_DUMP_FILE = ARGV[5]
+MAX_TABLES = ARGV[6]
 
-puts "#{DB_NAME} #{DB_HOST} #{DB_USER} #{DB_PW} #{DUMP_DIR} #{TABLES_TO_DUMP_FILE} #{S3_BUCKET}"
+puts "#{DB_NAME} #{DB_HOST} #{DB_USER} #{DB_PW} #{DUMP_DIR} #{TABLES_TO_DUMP_FILE}"
 
 TABLES_TO_DUMP = []
 set_cnt = 0
@@ -25,7 +25,7 @@ File.open(TABLES_TO_DUMP_FILE).each do |line|
     TABLES_TO_DUMP[set_cnt] << line << " " 
     table_cnt += 1
   end
-  if table_cnt > 2000
+  if table_cnt > MAX_TABLES.to_i
     set_cnt += 1
     table_cnt = 1
     TABLES_TO_DUMP[set_cnt] = ""
@@ -35,6 +35,4 @@ end
 TABLES_TO_DUMP.each_with_index do |tables_to_dump, index|
   puts "Dumping to #{DUMP_DIR}/#{DB_NAME}_skipped_#{index}.sql.gz"
   `mysqldump --single-transaction --quick --flush-logs -u #{DB_USER} -p#{DB_PW} -h #{DB_HOST} #{DB_NAME} #{tables_to_dump} | gzip -1 >  #{DUMP_DIR}/#{DB_NAME}_table_list_#{index}.sql.gz`
-  #puts "copying to $S3_BUCKET"
-  #`s3cmd --force put #{DUMP_DIR}/#{DB_NAME}_skipped.sql.gz s3://$S3_BUCKET/#{DB_NAME}_skipped.sql.gz`
 end
